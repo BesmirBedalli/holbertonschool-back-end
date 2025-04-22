47afa04f29d39424ab14fab1467116b5a4a049e3
BesmirBedalli <bbedalli1987@gmail.com>
2025-04-22 18:06:00 +0200

    backend all tasks

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -7,15 +7,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """display Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello1():
     """display HBNB"""
     return 'HBNB'
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def hello2(text):
